62b2a04e2bc1bb92500529136d3122af0f5495e1
Alex Rodenberg <arodenberg@danmagi.com>
2019-03-28 19:13:34 +0100

    g

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ pollen_index = {
 def index(req, resp):
     global last_query
     global last_updated
+    global pollen_values
     # Make sure we cache for at least X seconds at a time
     if last_query == "" or last_query < datetime.datetime.now() - datetime.timedelta(minutes=15):
         last_query = datetime.datetime.now()

a1b7b9077c4de50c7829deca41f4b5099904c685
Alex Rodenberg <arodenberg@danmagi.com>
2019-03-28 14:09:51 +0100

    responder

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ This was built for a free account on herokuapp.com
 There is some caching going on and simple javascript to query twitter dynamically on load. 
 This assures we get updated numbers after pageload if its not the latest.
 
-You will need ENVIRONMENT VARIABLES for twitter keys
+Dynaconf env variables:
+```
+DYNACONF_ACCESS_SECRET=
+DYNACONF_ACCESS_TOKEN=
+DYNACONF_CONSUMER_KEY=
+DYNACONF_CONSUMER_SECRET=
+DYNACONF_CACHE_TIMER=300
+```
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from flask import Flask, Blueprint, render_template, jsonify
+import responder
 import datetime
 import time
 import tweepy
 from tweepy import OAuthHandler
-from whitenoise import WhiteNoise
+from dynaconf import settings
 
-frontend = Blueprint('frontend', __name__)
+api = responder.API()
 last_query = ""
 last_consume = ""
 pollentypes = ['el', 'hassel', 'elm', 'birk', 'græs', 'bynke']
 pollendic = []
 
-
-@frontend.route('/')
-def index():
+@api.route('/')
+def index(req, resp):
     global last_query
     global last_updated
     global pollen_values
@@ -21,19 +20,19 @@ def index():
         # Make sure we have some data.
         check_twitter()
     # Make sure we cache for at least X seconds at a time
-    if last_query == "" or last_query < datetime.datetime.now() - datetime.timedelta(seconds=app.config['CACHE_TIMER']):
+    if last_query == "" or last_query < datetime.datetime.now() - datetime.timedelta(minutes=settings.CACHE_TIMER):
         last_query = datetime.datetime.now()
         last_updated, pollen_values = render_view()
-    return render_template('index.html', last_updated=last_updated, reload=time.time(), **pollen_values)
+    resp.html = api.template('index.html', last_updated=last_updated, reload=time.time(), **pollen_values)
 
 
-@frontend.route("/api/info")
-def api_info():
+@api.route("/api/info")
+def api_info(req, resp):
     global last_consume
     global last_updated
     global pollen_values
     # Consume twitter but only if not done within last X minutes
-    if last_consume == "" or last_consume < datetime.datetime.now() - datetime.timedelta(minutes=app.config['CONSUME_INTERVAL']):
+    if last_consume == "" or last_consume < datetime.datetime.now() - datetime.timedelta(minutes=settings.CACHE_TIMER):
         last_consume = datetime.datetime.now()
         check_twitter()
     last_updated, pollen_values = render_view()
@@ -42,16 +41,16 @@ def api_info():
         "last_updated": last_updated,
         **pollen_values
     }
-    return jsonify(info)
+    resp.media = info
 
 
 def check_twitter():
     """ Checks for new twitter entries """
     global pollendic
-    consumer_key = app.config['CONSUMER_KEY']
-    consumer_secret = app.config['CONSUMER_SECRET']
-    access_token = app.config['ACCESS_TOKEN']
-    access_secret = app.config['ACCESS_SECRET']
+    consumer_key = settings.CONSUMER_KEY
+    consumer_secret = settings.CONSUMER_SECRET
+    access_token = settings.ACCESS_TOKEN
+    access_secret = settings.ACCESS_SECRET
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
@@ -93,15 +92,19 @@ def check_twitter():
             for item in pollentypes:
                 for sublist in dataarray:
                     if sublist['pollen'] == item:
-                        ourpollen = {'created': str(created), 'tweet_id': tweet_id}
-                        ourpollen.update({'location': sublist['location'], 'pollen': sublist['pollen'], 'value': sublist['value']})
-
+                        ourpollen = {'created': str(created), 
+                                     'location': sublist['location'],
+                                     'pollen': sublist['pollen'],
+                                     'value': sublist['value']
+                                    }
                         pollendic.append(ourpollen)
+            # Break as soon as we have processed 1 tweet with pollendata
             break
 
 
 def render_view():
     pollen_values = {}
+    # Populate fields with 0 so we are not missing data in our html table.
     for pollen in pollentypes:
         pollen_values[pollen + '_øst'] = 0
         pollen_values[pollen + '_vest'] = 0
@@ -112,10 +115,5 @@ def render_view():
     return last_updated, pollen_values
 
 
-app = Flask(__name__)
-app.register_blueprint(frontend)
-app.config.from_object('config.default')
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    api.run()

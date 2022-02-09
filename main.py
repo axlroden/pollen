5858e5968b48da6fcf46654e735b66ffea4c9226
Alex Rodenberg <arodenberg@danmagi.com>
2022-02-09 01:54:19 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import responder
 import requests
+import json
 import sys
 from cachetools import cached, TTLCache
 
@@ -19,44 +20,50 @@ pollen_index = {
 @cached(cache)
 @api.route('/')
 def index(req, resp):
-    pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
-    resp.html = api.template('index.html', last_updated=pollen_data['48']['date'], **pollen_data)
+    date, pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
+    resp.html = api.template('index.html', last_updated=date, **pollen_data)
 
 @cached(cache)
 @api.route('/siri-east')
 def index(req, resp):
-    pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
-    resp.html = api.template('siri-east.html', last_updated=pollen_data['48']['date'], **pollen_data)
+    date, pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
+    resp.html = api.template('siri-east.html', last_updated=date, **pollen_data)
 
 @cached(cache)
 @api.route('/siri-west')
 def index(req, resp):
-    pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
-    resp.html = api.template('siri-west.html', last_updated=pollen_data['49']['date'], **pollen_data)
+    date, pollen_data = render_pollen('https://www.astma-allergi.dk/umbraco/Api/PollenApi/GetPollenFeed')
+    resp.html = api.template('siri-west.html', last_updated=date, **pollen_data)
 
 @cached(cache)
 def render_pollen(feed):
     pollen_values = {}
     try:
         r = requests.get(feed)
-        feed_values = r.json()
+        feed_values = json.loads(r.json())
+
     except requests.exceptions.RequestException as e:
         print(e)
         sys.stdout.flush()
         return "", pollen_values
-    for item in feed_values:
-        if item == '48':
-            location = 'øst'
-        elif item == '49':
-            location = 'vest'
-        else:
-            location = ''
-        for pollen in item['data']:
-            value = pollen['level']
-            if value == -1:
-                value = "0"
-            pollen_values['{0}_{1}'.format(pollen_index[pollen]['type'], location)] = value
-    return pollen_values
+    
+    location = "øst"
+    for item in feed_values["48"]["data"]:
+        if item == '44' or item == '45':
+            continue
+        value = feed_values["48"]["data"][item]['level']
+        if value == -1:
+            value = "0"
+        pollen_values['{0}_{1}'.format(pollen_index[item]["type"], location)] = value
+    location = "vest"
+    for item in feed_values["49"]["data"]:
+        if item == '44' or item == '45':
+            continue
+        value = feed_values["49"]["data"][item]['level']
+        if value == -1:
+            value = "0"
+        pollen_values['{0}_{1}'.format(pollen_index[item]["type"], location)] = value
+    return feed_values["48"]["date"], pollen_values
 
 if __name__ == "__main__":
     api.run()
